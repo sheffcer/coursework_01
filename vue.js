5e5d88e859de8d3790f2356e56d935d48b095277
Alex sheffcer <a.sheffcer@gmail.com>
2021-01-04 09:48:31 +0200

    added dynamic active class

--- vue.js
+++ vue.js
@@ -7,25 +7,30 @@ const App = {
     return {
       activeIndex: 0, // то, что позволяет определить текущий активный шиг,
       isActive: true,
-      steps: [
-        {title: 'Основы', 
-        text: 'В блоке вы познакомитесь со всеми основами Vue.js на практике. На протяжении блока мы напишем реактивное приложение, в процессе разработки которого разберем вся базу фреймворка.',
-         isActive: false
+      // isButtonDisabled: true,
+      steps: [{
+          title: 'Основы',
+          text: 'В блоке вы познакомитесь со всеми основами Vue.js на практике. На протяжении блока мы напишем реактивное приложение, в процессе разработки которого разберем вся базу фреймворка.',
+          isActive: true
         },
-        {title: 'Компоненты',
-         text: 'Один из самых важных блоков в курсе, где вы узнаете все о компонентах. В блоке мы напишем 2 разных приложения и создадим более 5 различных UI компонентов как в реальной разработке. Блок расскажет про абсолютно все составляющие, которые есть в компонентах: взаимодействие, slots, асинхронные и динамические компоненты и тонна примеров.',
-         isActive: true
+        {
+          title: 'Компоненты',
+          text: 'Один из самых важных блоков в курсе, где вы узнаете все о компонентах. В блоке мы напишем 2 разных приложения и создадим более 5 различных UI компонентов как в реальной разработке. Блок расскажет про абсолютно все составляющие, которые есть в компонентах: взаимодействие, slots, асинхронные и динамические компоненты и тонна примеров.',
+          isActive: false
         },
-        {title: 'Роутер', 
-        text: 'В данном блоке вы узнаете все о том, как работает мультиязычность во Vue. Мы создадим миниклон Gmail в данном блоке, где вы на практике увидите как работать с динамическим роутером.',
-        isActive: false
+        {
+          title: 'Роутер',
+          text: 'В данном блоке вы узнаете все о том, как работает мультиязычность во Vue. Мы создадим миниклон Gmail в данном блоке, где вы на практике увидите как работать с динамическим роутером.',
+          isActive: false
         },
-        {title: 'Vuex', 
-        text: 'В блоке вы узнаете абсолютно все про Vuex. Вы узнаете как работать с данными, какие есть лучшие практики по их программированию и структурированию. Все на практике.',
-        isActive: false
+        {
+          title: 'Vuex',
+          text: 'В блоке вы узнаете абсолютно все про Vuex. Вы узнаете как работать с данными, какие есть лучшие практики по их программированию и структурированию. Все на практике.',
+          isActive: false
         },
-        {title: 'Composition', 
-        text: 'Одним из наиболее важных обновлений в Vue 3 является появление альтернативного синтаксиса Composition API. В этом блоке вы узнаете все, чтобы полностью пользоваться данными синтаксисом на практических примерах. Помимо этого вы узнаете как работать совместно с Vue Router и Vuex.',
+        {
+          title: 'Composition',
+          text: 'Одним из наиболее важных обновлений в Vue 3 является появление альтернативного синтаксиса Composition API. В этом блоке вы узнаете все, чтобы полностью пользоваться данными синтаксисом на практических примерах. Помимо этого вы узнаете как работать совместно с Vue Router и Vuex.',
           isActive: false
         },
       ]
@@ -40,22 +45,51 @@ const App = {
     },
     nextOfFinish() {
       // кнопка вперед или закончить
+      this.steps[this.activeIndex].isActive = false
+      this.steps[this.activeIndex + 1].isActive = true
+      this.activeIndex = this.activeIndex + 1
+      console.log('enter pressed index: '+ this.steps[this.activeIndex].isActive)
     },
-    setActive(idx) {
+    setActive(index) {
       // когда нажимаем на определенный шаг
+      console.log(index)
+      // this.activeIndex = index
+      this.steps[this.activeIndex].isActive = false
+      this.steps[index].isActive = true
+      this.activeIndex = index
+
     }
   },
   computed: {
-    currentStep: function () {
+    filteredStep: function () {
       let allSteps = this.steps
-      let current = allSteps.filter((e) => {
+      let filtered = allSteps.filter((e) => {
         if (e.isActive === true) {
-           return e
+          return e
         }
-      
       })
-       console.log(current[0].title)
-      return current
+      // console.log(filtered[0].title)
+      return filtered
+    },
+    currentStep: function () {
+      let current = this.activeIndex
+      console.log(current)
+    },
+    islastStep: function () {
+      let last = this.steps.length - 1
+      console.log(last)
+      if (this.activeIndex === last) {
+        return true
+      } else {
+        return false
+      }
+    },
+    isButtonDisabled: function () {
+      if (this.activeIndex != 0) {
+        return false
+      } else {
+        return true
+      }
     }
     // тут стоит определить несколько свойств:
     // 1. текущий выбранный шаг
