6a45a31305e2123223ea61cfc4a3dc9cb1887a04
Alex sheffcer <a.sheffcer@gmail.com>
2021-01-06 11:12:18 +0200

    fix reset method

--- vue.js
+++ vue.js
@@ -6,87 +6,99 @@ const App = {
   data() {
     return {
       activeIndex: 0, // то, что позволяет определить текущий активный шиг,
-      // isActive: true,
-      // isDone: false,
-      // isButtonDisabled: true,
-      defaultSteps: [],
       steps: [{
-          title: 'Основы',
-          text: 'В блоке вы познакомитесь со всеми основами Vue.js на практике. На протяжении блока мы напишем реактивное приложение, в процессе разработки которого разберем вся базу фреймворка.',
-          isActive: true,
-          isDone: false
-        },
-        {
-          title: 'Компоненты',
-          text: 'Один из самых важных блоков в курсе, где вы узнаете все о компонентах. В блоке мы напишем 2 разных приложения и создадим более 5 различных UI компонентов как в реальной разработке. Блок расскажет про абсолютно все составляющие, которые есть в компонентах: взаимодействие, slots, асинхронные и динамические компоненты и тонна примеров.',
-          isDone: false
-        },
-        {
-          title: 'Роутер',
-          text: 'В данном блоке вы узнаете все о том, как работает мультиязычность во Vue. Мы создадим миниклон Gmail в данном блоке, где вы на практике увидите как работать с динамическим роутером.',
-          isDone: false
-        },
-        {
-          title: 'Vuex',
-          text: 'В блоке вы узнаете абсолютно все про Vuex. Вы узнаете как работать с данными, какие есть лучшие практики по их программированию и структурированию. Все на практике.',
-          isDone: false
-        },
-        {
-          title: 'Composition',
-          text: 'Одним из наиболее важных обновлений в Vue 3 является появление альтернативного синтаксиса Composition API. В этом блоке вы узнаете все, чтобы полностью пользоваться данными синтаксисом на практических примерах. Помимо этого вы узнаете как работать совместно с Vue Router и Vuex.',
-          isActive: false,
-          isDone: false
-        },
+        title: 'Основы',
+        text: 'В блоке вы познакомитесь со всеми основами Vue.js на практике. На протяжении блока мы напишем реактивное приложение, в процессе разработки которого разберем вся базу фреймворка.',
+        isActive: true,
+        isDone: false
+      },
+      {
+        title: 'Компоненты',
+        text: 'Один из самых важных блоков в курсе, где вы узнаете все о компонентах. В блоке мы напишем 2 разных приложения и создадим более 5 различных UI компонентов как в реальной разработке. Блок расскажет про абсолютно все составляющие, которые есть в компонентах: взаимодействие, slots, асинхронные и динамические компоненты и тонна примеров.',
+        isActive: false,
+        isDone: false
+      },
+      {
+        title: 'Роутер',
+        text: 'В данном блоке вы узнаете все о том, как работает мультиязычность во Vue. Мы создадим миниклон Gmail в данном блоке, где вы на практике увидите как работать с динамическим роутером.',
+        isActive: false,
+        isDone: false
+      },
+      {
+        title: 'Vuex',
+        text: 'В блоке вы узнаете абсолютно все про Vuex. Вы узнаете как работать с данными, какие есть лучшие практики по их программированию и структурированию. Все на практике.',
+        isActive: false,
+        isDone: false
+      },
+      {
+        title: 'Composition',
+        text: 'Одним из наиболее важных обновлений в Vue 3 является появление альтернативного синтаксиса Composition API. В этом блоке вы узнаете все, чтобы полностью пользоваться данными синтаксисом на практических примерах. Помимо этого вы узнаете как работать совместно с Vue Router и Vuex.',
+        isActive: false,
+        isDone: false
+      },
       ]
     }
   },
   methods: {
     prev() {
-      // когда нажимаем кнопку назад
-      // if (this.activeIndex != 0) {
       this.steps[this.activeIndex].isActive = false
       this.steps[this.activeIndex - 1].isActive = true
       this.activeIndex = this.activeIndex - 1
-      // }
+      this.steps.forEach((item, i) => {
+        if (i < this.activeIndex) {
+          return item.isDone = true
+        } else if (i > this.activeIndex) {
+          return item.isDone = false
+        }
+      })
     },
     reset() {
-      // начать заного
       if (this.activeIndex === this.last) {
-          // document.querySelector('.btn--reset').hidden = true
-          document.querySelector('.btn--prev').hidden = false
-          document.querySelector('.btn--next').hidden = false
-          document.querySelector('.btn--reset').hidden = true
-          this.activeIndex = 0
-          let done = this.steps.forEach((item, i) => {
-            if (i > 0) {
-              return item.isActive = false
-            } else if (i === 0) {
-              return item.isActive = true
-            }
-          })
+        document.querySelector('.btn--prev').hidden = false
+        document.querySelector('.btn--next').hidden = false
+        document.querySelector('.btn--reset').hidden = true
+        this.activeIndex = 0
+        let done = this.steps.forEach((item, i) => {
+          if (i > 0) {
+            item.isActive = false
+            item.isDone = false
+          } else if (i === 0) {
+            item.isActive = true
+          }
+          return item
+        })
       }
     },
     nextOfFinish() {
-      // кнопка вперед или закончить
       this.steps[this.activeIndex].isActive = false
       this.steps[this.activeIndex + 1].isActive = true
       this.activeIndex = this.activeIndex + 1
-      // console.log('enter pressed index: '+ this.steps[this.activeIndex].isActive)
+      this.steps.forEach((item, i) => {
+        if (i < this.activeIndex) {
+          return item.isDone = true
+        } else if (i > this.activeIndex) {
+          return item.isDone = false
+        }
+      })
     },
     setActive(index) {
-      // когда нажимаем на определенный шаг
-      // console.log(index)
-      // this.activeIndex = index
       this.steps[this.activeIndex].isActive = false
       this.steps[index].isActive = true
       this.activeIndex = index
+      this.steps.forEach((item, i) => {
+        if (i < this.activeIndex) {
+          return item.isDone = true
+        } else if (i > this.activeIndex) {
+          return item.isDone = false
+        }
+      })
+
     },
     continueStep() {
       document.querySelector('.btn--prev').hidden = true
       document.querySelector('.btn--next').hidden = true
       document.querySelector('.btn--continue').hidden = true
       document.querySelector('.btn--reset').hidden = false
-      console.log('continue?')
     }
   },
   computed: {
@@ -97,19 +109,16 @@ const App = {
           return e
         }
       })
-      // console.log(filtered[0].title)
       return filtered
     },
     currentStep() {
       return this.activeIndex
-      },
-    last () {
-        return this.steps.length - 1
+    },
+    last() {
+      return this.steps.length - 1
     },
 
     islastStep() {
-      // let last = this.steps.length - 1
-      // console.log(last)
       if (this.currentStep === this.last) {
         return true
       } else {
@@ -123,19 +132,7 @@ const App = {
         return true
       }
     },
-    isDone() {
-      let done = this.steps.map((e) => {
-        if (e.index < this.currentStep) {
-          e.isDone = true
-        } else if (e.index >= this.currentStep) {
-          e.isDone = false
-        }
-      })
-      // return done
-      // return(done)
-      // console.log(done)
-    }
-    // тут стоит определить несколько свойств:
+     // тут стоит определить несколько свойств:
     // 1. текущий выбранный шаг
     // 2. выключена ли кнопка назад
     // 3. находимся ли мы на последнем шаге
